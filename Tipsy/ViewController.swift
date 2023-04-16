eb45b987dd12abc21b77bcbe01312091f70219b3
Vasilii <shores.freezer0u@icloud.com>
2023-04-16 10:55:31 +1000

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by VASILY IKONNIKOV on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
